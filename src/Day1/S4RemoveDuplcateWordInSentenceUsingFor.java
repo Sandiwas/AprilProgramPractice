57237810601e232990b44b6b653e56261da95be7
sandiwas <harrycode9@gmail.com>
2026-04-01 16:20:52 +0530

    added RemoveDuplcateWordInSentence Using For using for loop program

---
+++ src/Day1/S4RemoveDuplcateWordInSentenceUsingFor.java
@@ -0,0 +1,26 @@
+package Day1;
+
+public class S4RemoveDuplcateWordInSentenceUsingFor {
+	public static void main(String[] args) {
+		String str = "Sandip gahudas GAHUDAS wasekar Sandip wasekar";
+		String[] arr = str.split(" ");
+		String result = "";
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equals("0")) {
+				continue;
+			}
+			for (int j = i + 1; j <arr.length; j++) {
+				if (arr[i].equalsIgnoreCase(arr[j])) {
+					arr[j] = "0";
+				}
+			}
+			if (!arr[i].equals("0")) {
+				result = result + arr[i] + " ";
+			}
+
+		}
+		System.out.println(result);
+	}
+}
+//Sandip gahudas wasekar 
