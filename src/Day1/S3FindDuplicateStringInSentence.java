d65ec26c786ebdf8c521dc02a03d2929ebe9ad7e
sandiwas <harrycode9@gmail.com>
2026-04-01 16:02:17 +0530

    added FindDuplicateStringInSentence using for loop program

---
+++ src/Day1/S3FindDuplicateStringInSentence.java
@@ -0,0 +1,30 @@
+package Day1;
+
+public class S3FindDuplicateStringInSentence {
+	public static void main(String[] args) {
+		
+		String str = "Sandip gahuadas Wasekar sandip";
+		String[] arr = str.split(" ");
+		
+		for(int i=0;i<arr.length;i++) {
+			int count=1;
+		
+			if(arr[i].equals("0")) {
+				continue;
+			}
+			
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i].equalsIgnoreCase(arr[j])) {
+					count++;
+					arr[j]="0";
+				}
+			}
+			
+			if(count>1 && !arr[i].equals("0")) {
+				System.out.println(arr[i]+" = "+count);
+			}
+		}
+	}
+}
+
+//Sandip = 2
