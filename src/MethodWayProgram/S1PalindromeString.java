ecb952503a73c733692e9966e064007b2d006046
sandiwas <harrycode9@gmail.com>
2026-04-01 13:50:55 +0530

    commited methodWay palindrome program

---
+++ src/MethodWayProgram/S1PalindromeString.java
@@ -0,0 +1,38 @@
+package MethodWayProgram;
+
+public class S1PalindromeString {
+	public static void main(String[] args){
+		
+	    String str="Radar";
+		str=removeSpaceMakeLowerCase(str);
+       System.out.println(isPalindrome(str));
+	}
+	
+	
+	public static String removeSpaceMakeLowerCase(String str){
+		char[] arr=str.toCharArray();
+		String result="";
+		for(int i=0;i<arr.length;i++){
+			if(arr[i] != ' '){
+				result+=arr[i];
+			}
+		}
+		return result.toLowerCase();
+	}
+	
+	public static String reverseString(String str){
+	String result="";
+	for(int i=str.length()-1;i>=0;i--){
+		result=result+str.charAt(i);
+	}
+	return result;
+	}
+	
+public static String isPalindrome(String str){
+	if(str.equals(reverseString(str))){
+		return "Given string is plaindrome";
+	}else{
+		return "Given string not is plaindrome";
+	}
+  }
+}
