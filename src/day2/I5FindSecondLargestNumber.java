9037dc9c60b895aa2851183dcb1c16f6bbe9fedf
sandiwas <harrycode9@gmail.com>
2026-04-02 18:42:34 +0530

    added find third largest number in array program

--- src/day2/I5FindSecondLargestNumber.java
+++ src/day2/I5FindSecondLargestNumber.java
@@ -2,24 +2,31 @@ package day2;
 
 public class I5FindSecondLargestNumber {
 	public static void main(String[] args) {
-		int arr[] = { 10, 15, 20, 20, 30, 10, 20 };
-		if (arr.length < 2) {
+		int arr[] = { 10, 15, 20, 25, 30, 10, 26 };
+		if (arr.length < 3) {
 			System.out.print("Invalid input");
 		}
 		int largest = Integer.MIN_VALUE;
-		int secondLargest = Integer.MIN_VALUE;
+		int second = Integer.MIN_VALUE;
+		int third = Integer.MIN_VALUE;
+
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] > largest) {
-				secondLargest = largest;
+				second = largest;
+				third = second;
 				largest = arr[i];
-			} else if (arr[i] > secondLargest && arr[i] != largest) {
-				secondLargest = arr[i];
-			}
+			} 
+			else if (arr[i] > second && arr[i] != largest) {
+				third = second;
+				second = arr[i];
+			} 
+			else if (arr[i] > third && arr[i] != second && arr[i] != largest)
+				third = arr[i];
 		}
-		if (secondLargest == Integer.MIN_VALUE) {
+		if (third == Integer.MIN_VALUE) {
 			System.out.println("No Second largest number");
 		} else {
-			System.out.println(secondLargest);
+			System.out.println(third);
 		}
 	}
 }
