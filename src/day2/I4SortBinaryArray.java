6bc5ce5269b62fb3d5c52cc512624a489c65d984
sandiwas <harrycode9@gmail.com>
2026-04-02 20:58:59 +0530

    rename  I3BubbleSort.java , I4SortBinaryArray.java../methodWay/P2PalindromeString.java and Added new program for to find the duplicate char using for loop

--- src/day2/I4SortBinaryArray.java
+++ src/day2/I4SortBinaryArray.java
@@ -3,23 +3,20 @@ package day2;
 import java.util.Arrays;
 
 public class I4SortBinaryArray {
-
 	public static void main(String[] args) {
-		int[] arr = { 5, 3, 4, 2 };
-		System.out.print(Arrays.toString(sortArray(arr)));
-	}
-
-	public static int[] sortArray(int[] arr) {
-		int n = arr.length;
-		for (int i = 0; i < n - 1; i++) {
-			for (int j = 0; j < n - i - 1; j++) {
-				if (arr[j] > arr[j + 1]) {
-					int temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-				}
+		int[] arr = { 0, 0, 0, 1, 0, 1, 0, 1 };
+		int zeroCount = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				zeroCount++;
 			}
 		}
-		return arr;
+		for (int i = 0; i < zeroCount; i++) {
+			arr[i] = 0;
+		}
+		for (int i = zeroCount; i < arr.length; i++) {
+			arr[i] = 1;
+		}
+		System.out.println(Arrays.toString(arr));
 	}
 }
