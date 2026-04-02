6fb69a08248791b19de3dfdc34a0e004b40f5c88
sandiwas <harrycode9@gmail.com>
2026-04-02 18:49:47 +0530

    commited rename file

--- src/day2/I5FindSecondLargestNumber.java
+++ src/day2/I5FindSecondLargestNumber.java
@@ -2,31 +2,28 @@ package day2;
 
 public class I5FindSecondLargestNumber {
 	public static void main(String[] args) {
-		int arr[] = { 10, 15, 20, 25, 30, 10, 26 };
-		if (arr.length < 3) {
-			System.out.print("Invalid input");
+		int arr[] = { 10, 15, 20, 20, 30, 10, 20 };
+		if (arr.length < 2) {
+			System.out.println("Invalid input");
 		}
+
 		int largest = Integer.MIN_VALUE;
 		int second = Integer.MIN_VALUE;
-		int third = Integer.MIN_VALUE;
 
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] > largest) {
 				second = largest;
-				third = second;
 				largest = arr[i];
-			} 
-			else if (arr[i] > second && arr[i] != largest) {
-				third = second;
+			} else if (arr[i] > second && arr[i] != largest) {
 				second = arr[i];
-			} 
-			else if (arr[i] > third && arr[i] != second && arr[i] != largest)
-				third = arr[i];
+			}
 		}
-		if (third == Integer.MIN_VALUE) {
-			System.out.println("No Second largest number");
+
+		if (second == Integer.MIN_VALUE) {
+			System.out.println("No second largest number");
 		} else {
-			System.out.println(third);
+			System.out.println(second);
 		}
+
 	}
 }
