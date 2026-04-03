08fde9f4cdc0006dd35244cf0100be4b2ce7e070
sandiwas <harrycode9@gmail.com>
2026-04-03 13:27:03 +0530

    added count duplicate element in array

---
+++ src/day2/II1CountDuplicateElementUsingFor.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class II1CountDuplicateElementUsingFor {
+	public static void main(String[] args) {
+		int[] arr = { 10, 20, 30, 10, 40, 50, 20, 30, 10 };
+		for (int i = 0; i < arr.length; i++) {
+			int count = 1;
+			if (arr[i] == -1) {
+				continue;
+			}
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					count++;
+					arr[j] = -1;
+				}
+			}
+			if (count > 1 && arr[i] != -1) {
+				System.out.println(arr[i] + " = " + count);
+			}
+		}
+	}
+}
