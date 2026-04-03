bb2901add5bec348788404919c5b8cad7cbcdcfa
sandiwas <harrycode9@gmail.com>
2026-04-03 23:45:25 +0530

    added get Alternet alphabet char program

---
+++ src/day1/S6AlternateAtoZ.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class S6AlternateAtoZ {
+	public static void main(String[] args) {
+		for (char ch = 'a'; ch <= 'z'; ch += 2) {
+			System.out.print(ch + " ");
+
+		}
+	}
+}
