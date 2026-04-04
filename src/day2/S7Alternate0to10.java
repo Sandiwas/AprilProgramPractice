ee96039faefea952815dfa1bb3fad224a7b3b9c4
sandiwas <harrycode9@gmail.com>
2026-04-04 21:36:25 +0530

    added find alternet 0 to 9 unicode value program

---
+++ src/day2/S7Alternate0to10.java
@@ -0,0 +1,9 @@
+package day2;
+
+public class S7Alternate0to10 {
+	public static void main(String[] args) {
+		for (char ch = '0'; ch <= '9'; ch++) {
+			System.out.println(ch+ " = " + (int) ch);
+		}
+	}
+}
