b221bf31fb6d416e6e25a51769efede208175b9a
sandiwas <harrycode9@gmail.com>
2026-04-04 22:32:45 +0530

    print Acending and Decending order String using Bubble sort

---
+++ src/day2/A1AscendingAndDescendingString.java
@@ -0,0 +1,44 @@
+package day2;
+
+
+public class A1AscendingAndDescendingString {
+	public static void main(String[] args) {
+		String str = "ajdhfuwykdADFNDJ";
+		
+		System.out.println("Ascending = "+sortAscending(str));
+		System.out.println("Descending = "+sortDescending(str));
+		
+	}
+	
+	public static String sortAscending(String str) {
+		char[] arr = str.toCharArray();
+		int n = arr.length;
+
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					char temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		return new String(arr);
+	}
+	
+	public static String sortDescending(String str) {
+		char[] arr = str.toCharArray();
+		int n = arr.length;
+
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - 1 - i; j++) {
+				if (arr[j] < arr[j + 1]) {
+					char temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		return new String(arr);
+	}
+}
