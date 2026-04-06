fcd7ea42e2aef7be424c2318d07667e241f1b151
sandiwas <harrycode9@gmail.com>
2026-04-06 14:44:29 +0530

    added check Armstring number program

---
+++ src/day3/S_11_ArmstrongNumber.java
@@ -0,0 +1,30 @@
+package day3;
+
+public class S_11_ArmstrongNumber {
+
+	public static void main(String[] args) {
+		int a;
+		int n = 407;
+		int c = 0;
+		int temp = n;
+		while (n > 0) {
+			a = n % 10;
+			n = n / 10;
+			c = c + (a * a * a);
+		}
+		if (temp == c) {
+			System.out.println(temp + " is Armstring number");
+		} else {
+			System.out.println(temp + " is not Armstring number");
+		}
+
+	}
+
+}
+//
+//1634
+//8208
+//9474
+//1 2 3 4 5 6 7 8 9 153 370 371 407
+
+//o/p : 407 is Armstring number
