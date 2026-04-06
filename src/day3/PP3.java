4ee405b5966f965b39b0160fb67d9ced0a10b027
sandiwas <harrycode9@gmail.com>
2026-04-06 17:44:50 +0530

    added PP3 pattern program

--- src/day3/PP3.java
+++ src/day3/PP3.java
@@ -1,12 +1,12 @@
 package day3;
 
-public class PP1 {
+public class PP3 {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 4; i++) {
-			for (int j = 1; j <= 4; j++) {
+			for (int j = 4; j >= i;j--) {
 				System.out.print("*");
 			}
-			System.out.println(" ");
+			System.out.println();
 		}
 	}
 }
