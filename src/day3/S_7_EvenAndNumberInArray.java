1b5b2016ac0127c2044546304b00485a26ba0eaf
sandiwas <harrycode9@gmail.com>
2026-04-06 12:37:17 +0530

    find even and odd number using for loop DSA

---
+++ src/day3/S_7_EvenAndNumberInArray.java
@@ -0,0 +1,40 @@
+package day3;
+
+import java.util.Arrays;
+
+public class S_7_EvenAndNumberInArray {
+	public static void main(String[] args) {
+		int[] arr = { 5, 6, 4, 3, 2, 7, 1 };
+
+		int evenCount = 0;
+		int oddCount = 0;
+
+		// Step 1: Count even and odd
+		for (int num : arr) {
+			if (num % 2 == 0) {
+				evenCount++;
+			} else {
+				oddCount++;
+			}
+		}
+
+		int e = 0;
+		int o = 0;
+		
+		// Step 2: Create arrays of exact size
+		int[] even = new int[evenCount];
+		int[] odd = new int[oddCount];
+
+		// Step 3: Store values
+		for (int num : arr) {
+			if (num % 2 == 0) {
+				even[e++] = num;
+			} else {
+				odd[o++] = num;
+			}
+		}
+		// Step 4: Print result
+		System.out.println("Even numbers: " + Arrays.toString(even));
+		System.out.println("Odd numbers: " + Arrays.toString(odd));
+	}
+}
