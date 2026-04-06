a728b5dea1c5368a7fe8cae675a179155b0284d1
sandiwas <harrycode9@gmail.com>
2026-04-06 12:08:22 +0530

    Swap number using 3rd variable

---
+++ src/day3/S_4_SwapTwoNumberWith3rdVar.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class S_4_SwapTwoNumberWith3rdVar {
+	public static void main(String[] args) {
+		int x = 100;
+		int y = 200;
+		int z;
+		z = x;
+		x = y;
+		y = z;
+
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+	}
+}
