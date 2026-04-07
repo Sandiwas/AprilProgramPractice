251b293dbd8bbd2cb397a7c8ea055f29bcf50e7c
sandiwas <harrycode9@gmail.com>
2026-04-07 13:38:04 +0530

    added PP18 program

---
+++ src/day3/PP18.java
@@ -0,0 +1,16 @@
+package day3;
+
+public class PP18 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 3; j >= i; j--) {
+				System.out.print(" ");
+			}
+			char ch = (i % 2 == 0) ? '*' : '&';
+			for (int k = 1; k <= i; k++) {
+				System.out.print(ch);
+			}
+			System.out.println(" ");
+		}
+	}
+}
