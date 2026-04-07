22c1ba94ace195b25eb2f4028ad1b9917a35bd69
sandiwas <harrycode9@gmail.com>
2026-04-07 15:04:01 +0530

    added PP29 program

---
+++ src/day3/PP29.java
@@ -0,0 +1,20 @@
+package day3;
+
+public class PP29 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 2; j <= i; j++) {
+				System.out.print(" ");
+			}
+			char ch = (i % 2 == 0) ? '*' : '&';
+			for (int k = 4; k >= i; k--) {
+				System.out.print(ch);
+			}
+			char ch1 = (i % 2 == 0) ? '*' : '&';
+			for (int l = 3; l >= i; l--) {
+				System.out.print(ch1);
+			}
+			System.out.println();
+		}
+	}
+}
