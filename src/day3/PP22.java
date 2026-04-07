6b33dfcd2f9f354991d4c0d6c3f992978d2cba63
sandiwas <harrycode9@gmail.com>
2026-04-07 13:55:34 +0530

    added PP22 program

---
+++ src/day3/PP22.java
@@ -0,0 +1,20 @@
+package day3;
+
+public class PP22 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 2; j <= i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 4; k >= i; k--) {
+				if (i % 2 == 0) {
+					System.out.print("*");
+				} else {
+					System.out.print("&");
+				}
+
+			}
+			System.out.println("");
+		}
+	}
+}
