6f71da5db3ad33df2f5b97151692e05810e33bff
sandiwas <harrycode9@gmail.com>
2026-04-07 10:54:32 +0530

    added PP12 pattern pragram

---
+++ src/day3/PP12.java
@@ -0,0 +1,23 @@
+package day3;
+
+public class PP12 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			if (i % 2 == 0) {
+				for (int j = 1; j <= 4; j++) {
+					System.out.print("*");
+				}
+			} else {
+				for (int j = 1; j <= 4; j++) {
+					System.out.print("&");
+				}
+			}
+			System.out.println("");
+		}
+	}
+}
+
+//&&&&
+//****
+//&&&&
+//****
