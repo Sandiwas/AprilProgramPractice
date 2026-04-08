09050b74d3ec1301713ab64a933be6e1cfd275f2
sandiwas <harrycode9@gmail.com>
2026-04-08 13:17:46 +0530

    added  PD5_DuplicateElementInUsingSet1.java push

---
+++ src/day4/PD5_DuplicateElementInUsingSet1.java
@@ -0,0 +1,17 @@
+package day4;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class PD5_DuplicateElementInUsingSet1 {
+	public static void main(String[] args) {
+		String str="programming";
+		char[] arr=str.toCharArray();
+		Set<Character> mySet = new LinkedHashSet<Character>();
+		for (char ele : arr)
+			if (!mySet.add(ele)) {
+				System.out.println(ele);
+			}
+		System.out.println(mySet);
+	}
+}
