4dd0dcee9ebf555d661f57f961dfe57ebc1ed240
sandiwas <harrycode9@gmail.com>
2026-04-08 17:16:56 +0530

    added PD5_DuplicateElementInUsingSet1.java modified program

--- src/day4/PD5_DuplicateElementInUsingSet1.java
+++ src/day4/PD5_DuplicateElementInUsingSet1.java
@@ -5,13 +5,20 @@ import java.util.Set;
 
 public class PD5_DuplicateElementInUsingSet1 {
 	public static void main(String[] args) {
-		String str="programming";
-		char[] arr=str.toCharArray();
+		String str = "programming";
+		char[] arr = str.toCharArray();
 		Set<Character> mySet = new LinkedHashSet<Character>();
 		for (char ele : arr)
 			if (!mySet.add(ele)) {
 				System.out.println(ele);
 			}
 		System.out.println(mySet);
+		
+		// Convert Set to String
+		String result = "";
+		for (char ch : mySet) {
+			result = result + ch;
+		}
+		System.out.println(result);
 	}
 }
