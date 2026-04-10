9c3e9af5475072b46a59cecb2a8c46c6d8cb402f
sandiwas <harrycode9@gmail.com>
2026-04-10 12:36:15 +0530

    fix logic and added seprate methid to check sting is anangram or not

--- src/methodWay/P3AnagramString.java
+++ src/methodWay/P3AnagramString.java
@@ -4,20 +4,26 @@ public class P3AnagramString {
 	public static void main(String[] args) {
 		String str1 = "Army";
 		String str2 = "Mary";
+		
 		str1 = removeSpaceMakeLowerCase(str1);
 		str2 = removeSpaceMakeLowerCase(str2);
 
 		char[] arr1 = str1.toCharArray();
 		char[] arr2 = str2.toCharArray();
+		
 		sortArray(arr1);
 		sortArray(arr2);
+		
+		isAnagram(arr1, arr2);
 
+	}
+
+	public static void isAnagram(char[] arr1, char[] arr2) {
 		if (isEuqlas(arr1, arr2)) {
 			System.out.println("Given String is Anagram");
 		} else {
 			System.out.println("Givem String is not palindrome");
 		}
-
 	}
 
 	public static boolean isEuqlas(char[] arr1, char[] arr2) {
@@ -36,7 +42,7 @@ public class P3AnagramString {
 		int n = arr.length;
 		for (int i = 0; i < n - 1; i++) {
 			for (int j = 0; j < n - i - 1; j++) {
-				if (arr[i] > arr[j]) {
+				if (arr[i] > arr[j+1]) {
 					char temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
