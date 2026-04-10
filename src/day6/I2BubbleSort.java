dcb0b54e0ab86bfb32c0a72b5f07aea0cc02e819
sandiwas <harrycode9@gmail.com>
2026-04-10 15:36:02 +0530

    added Bubble sort

---
+++ src/day6/I2BubbleSort.java
@@ -0,0 +1,23 @@
+package day6;
+
+import java.util.Arrays;
+
+public class I2BubbleSort {
+	public static void main(String[] args) {
+		int[] arr = { 5, 3, 4, 2 };
+		System.out.println(sortArray(arr));
+	}
+	public static String sortArray(int[] arr) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		return Arrays.toString(arr);
+	}
+}
