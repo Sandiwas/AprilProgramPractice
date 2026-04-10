f316b525ad63aedb709fd365c1131cf8afb5bdcf
sandiwas <harrycode9@gmail.com>
2026-04-10 14:13:20 +0530

    added P7SwapUsingThirdVaribale.java program

---
+++ src/day6/P7SwapUsingThirdVaribale.java
@@ -0,0 +1,17 @@
+package day6;
+
+public class P7SwapUsingThirdVaribale {
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		String s2 = "World";
+		String s3;
+
+		s3 = s1;
+		s1 = s2;
+		s2 = s3;
+
+		System.out.println("s1 = " + s1);
+		System.out.println("s2 = " + s2);
+
+	}
+}
