7da521497060862338dac4fe115179e576b62d48
sandiwas <harrycode9@gmail.com>
2026-04-10 15:58:55 +0530

    added sort binary array

---
+++ src/day6/I4SortBinaryArray.java
@@ -0,0 +1,23 @@
+package day6;
+
+import java.util.Arrays;
+
+public class I4SortBinaryArray {
+	public static void main(String[] args) {
+		int[] arr = { 0, 0, 0, 1, 0, 1, 0, 1 };
+		int zeroCount = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				zeroCount++;
+			}
+		}
+		for (int i = 0; i < zeroCount; i++) {
+			arr[i] = 0;
+		}
+		for (int i = zeroCount; i < arr.length; i++) {
+			arr[i] = 1;
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+}
