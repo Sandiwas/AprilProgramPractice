117ca616ec2a2fb7dcfdcedcb0a351c80fae7ef0
sandiwas <harrycode9@gmail.com>
2026-04-10 12:05:16 +0530

    reverse String usning StringBuffer

---
+++ src/day6/P3ReverseSentence.java
@@ -0,0 +1,17 @@
+package day6;
+
+public class P3ReverseSentence {
+	public static void main(String[] args) {
+		String str = "Automation is fun";
+		System.out.println(reverseSentence(str));
+	}
+
+	public static String reverseSentence(String str) {
+		String[] arr = str.split(" ");
+		String result = "";
+		for (int i = arr.length - 1; i >= 0; i--) {
+			result = result + arr[i] + " ";
+		}
+		return result;
+	}
+}
---
+++ src/day6/P4ReverseStringSB.java
@@ -0,0 +1,10 @@
+package day6;
+
+public class P4ReverseStringSB {
+	public static void main(String[] args) {
+		String str = "Automation";
+		StringBuffer sb = new StringBuffer(str);
+		
+		System.out.println(sb.reverse());
+	}
+}
