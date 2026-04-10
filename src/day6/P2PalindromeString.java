ea44c3d95f44a5c3de0c1a14fec31ef14ee1cb01
sandiwas <harrycode9@gmail.com>
2026-04-10 12:02:07 +0530

    check palindrome string program

---
+++ src/day6/P2PalindromeString.java
@@ -0,0 +1,36 @@
+package day6;
+
+public class P2PalindromeString {
+	public static void main(String[] args) {
+		String str = "radAr";
+		str = removeSpaceMakeLowerCase(str);
+		isPalindromeString(str);
+	}
+
+	public static void isPalindromeString(String str) {
+		if (str.equals(revreseString(str))) {
+			System.out.println("Given String is plaindrome String");
+		} else {
+			System.out.println("Given String is not palindrome String");
+		}
+	}
+
+	public static String revreseString(String str) {
+		String result = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			result = result + str.charAt(i);
+		}
+		return result;
+	}
+
+	public static String removeSpaceMakeLowerCase(String str) {
+		char[] arr = str.toCharArray();
+		String result = "";
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != ' ') {
+				result = result + arr[i];
+			}
+		}
+		return result.toLowerCase();
+	}
+}
