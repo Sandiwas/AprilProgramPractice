7109d32c27ec46926cb7e8c8caa52ff7f7528854
sandiwas <harrycode9@gmail.com>
2026-04-10 14:14:09 +0530

    added P8SwapWithoutUsingThirdVaribale.java program

---
+++ src/day6/P8SwapWithoutUsingThirdVaribale.java
@@ -0,0 +1,16 @@
+package day6;
+
+public class P8SwapWithoutUsingThirdVaribale {
+	public static void main(String[] args) {
+
+		String s1 = "Hello";
+		String s2 = "world";
+
+		s1 = s1 + s2;
+		s2 = s1.substring(0, s1.length() - s2.length());
+		s1 = s1.substring(s2.length());
+		
+		System.out.println("s1 = " + s1);
+		System.out.println("s2 = " + s2);
+	}
+}
