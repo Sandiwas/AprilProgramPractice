b373846378d23897239868e7f219ee879b1a3e57
sandiwas <harrycode9@gmail.com>
2026-04-10 17:57:30 +0530

    added find missing number in array

---
+++ src/day6/I8MissingNumberSort.java
@@ -0,0 +1,31 @@
+package day6;
+
+public class I8MissingNumberSort {
+	public static void main(String[] args) {
+		int[] arr = { 5, 4, 1,3 };
+		sortArray(arr);
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != i + 1) {
+				System.out.println("Missing number is " + (i + 1));
+				return;
+			}
+		}
+		System.out.println("No missing number in array");
+	}
+
+	public static void sortArray(int[] arr) {
+		int n = arr.length;
+
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+
+		}
+	}
+}
