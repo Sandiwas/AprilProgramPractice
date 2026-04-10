0d3b958b05e3bb6198f058f06a70bb990e2b0abf
sandiwas <harrycode9@gmail.com>
2026-04-10 14:29:39 +0530

    added P8SwapWithoutUsingThirdVaribale.java program

--- src/day6/P9ReversEachStringinSentence.java
+++ src/day6/P9ReversEachStringinSentence.java
@@ -1,5 +1,22 @@
 package day6;
 
 public class P9ReversEachStringinSentence {
+	public static void main(String[] args) {
+		String str = "Automation  is fun";
+		System.out.println(reverseString(str));
+	}
 
+	public static String reverseString(String str) {
+		String[] arr = str.split(" ");
+		String result = "";
+		for (int i = 0; i < arr.length; i++) {
+			String word = arr[i];
+			String reverseWord = "";
+			for (int j = word.length() - 1; j >= 0; j--) {
+				reverseWord = reverseWord + word.charAt(j);
+			}
+			result = result + reverseWord + " ";
+		}
+		return result;
+	}
 }
