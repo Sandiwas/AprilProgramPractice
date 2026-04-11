4e909af0831837f8467e52a46428eb2293234e8d
sandiwas <harrycode9@gmail.com>
2026-04-11 10:40:22 +0530

    find missing number in array

---
+++ src/day7/I7MissingNumberSort1.java
@@ -0,0 +1,20 @@
+package day7;
+
+public class I7MissingNumberSort1 {
+	public static void main(String[] args) {
+
+		int[] arr = { 5, 4, 2, 3 };
+
+		int n = arr.length + 1;
+		int sum1 = 0;
+		int sum2 = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum1 = sum1 + arr[i];
+		}
+
+		for (int i = 1; i <= n; i++) {
+			sum2 = sum2 + i;
+		}
+		System.out.println(sum2 - sum1);
+	}
+}
