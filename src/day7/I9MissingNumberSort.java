30ba456206b11e8479507309c27ff8b4fafa3d0d
sandiwas <harrycode9@gmail.com>
2026-04-11 11:07:42 +0530

    find missing number

--- src/day7/I9MissingNumberSort.java
+++ src/day7/I9MissingNumberSort.java
@@ -3,7 +3,7 @@ package day7;
 public class I9MissingNumberSort {
 
 	public static void main(String[] args) {
-		int[] arr = { 5, 4, 1, 2 };
+		int[] arr = { 5, 4, 1,1, 2 };
 		sortArray(arr);
 		int expected = 1;
 		for (int i = 0; i < arr.length; i++) {
