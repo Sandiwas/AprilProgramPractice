2bd5d869bbe7d65b6e6264a960adff0550b02f9c
sandiwas <harrycode9@gmail.com>
2026-04-12 21:57:18 +0530

    added revrse String using recursion

---
+++ src/day7/P5ReverseStringUsingRecurtion.java
@@ -0,0 +1,16 @@
+package day7;
+
+public class P5ReverseStringUsingRecurtion {
+	public static void main(String[] args) {
+		String str = "java";
+		System.out.println("Given String is " + str + " output is " + reverseString(str));
+	}
+
+	public static String reverseString(String str) {
+		if (str.length() == 0) {
+			return str;
+		}
+
+		return reverseString(str.substring(1)) + str.charAt(0);
+	}
+}
