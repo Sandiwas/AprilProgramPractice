aa73f817490a47ac6d5b382ec1193011c8a8dbd9
sandiwas <harrycode9@gmail.com>
2026-04-12 22:44:38 +0530

    added sort acending and decending order of String

---
+++ src/day7/A1AscendingAndDescendingString.java
@@ -0,0 +1,42 @@
+package day7;
+
+
+public class A1AscendingAndDescendingString {
+	public static void main(String[] args) {
+		String str = "ajdhfuwykdADFNDJ";
+
+		System.out.println(sortAcendingOrder(str));
+		System.out.println(sortDecendingOrder(str));
+
+	}
+
+	public static String sortAcendingOrder(String str) {
+		char[] arr = str.toCharArray();
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					char temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		return new String(arr);
+	}
+
+	public static String sortDecendingOrder(String str) {
+		char[] arr = str.toCharArray();
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] < arr[j + 1]) {
+					char temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		return new String(arr);
+	}
+}
