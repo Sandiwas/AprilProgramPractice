25d1d5003d1ad9f75a50c2d15c0eb75e4aea3ebe
sandiwas <harrycode9@gmail.com>
2026-04-13 10:40:12 +0530

    added remove duplicate element in array

--- src/day8/II2RemoveDuplicateNumberUsingFor.java
+++ src/day8/II2RemoveDuplicateNumberUsingFor.java
@@ -1,25 +1,24 @@
 package day8;
 
-public class II1CountDuplicateElementUsingFor {
+public class II2RemoveDuplicateNumberUsingFor {
 	public static void main(String[] args) {
 		int[] arr = { 10, 20, 30, 10, 40, 50, 20, 30, 10 };
-		countDuplicateElement(arr);
+		removeDuplicateElement(arr);
 	}
 
-	public static void countDuplicateElement(int[] arr) {
+	public static void removeDuplicateElement(int[] arr) {
+
 		for (int i = 0; i < arr.length; i++) {
-			int count = 1;
 			if (arr[i] == -1) {
 				continue;
 			}
 			for (int j = i + 1; j < arr.length; j++) {
 				if (arr[i] == arr[j]) {
-					count++;
 					arr[j] = -1;
 				}
 			}
-			if (count > 1 && arr[i] != -1) {
-				System.out.println(arr[i] + " = " + count);
+			if (arr[i] != -1) {
+				System.out.print(arr[i] + " ");
 			}
 		}
 
