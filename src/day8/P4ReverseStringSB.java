890c89f3e6bdc9b275fe26a220962d4fb24ef301
sandiwas <harrycode9@gmail.com>
2026-04-13 12:07:55 +0530

    added revrese string program

---
+++ src/day8/P4ReverseStringSB.java
@@ -0,0 +1,10 @@
+package day8;
+
+public class P4ReverseStringSB {
+	public static void main(String[] args) {
+		String str = "java";
+		StringBuffer sb=new StringBuffer(str);
+		System.out.println(sb.reverse());
+
+	}
+}
