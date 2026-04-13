07a1a33aae7a0c41b52e1b93c3c8f7e19868e44b
sandiwas <harrycode9@gmail.com>
2026-04-13 13:52:21 +0530

    make upper case String

---
+++ src/day8/C2MakeUpperCaseString.java
@@ -0,0 +1,19 @@
+package day8;
+
+public class C2MakeUpperCaseString {
+	public static void main(String[] args) {
+		String str = "AuToMaTiOn";
+		String result = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+
+			if (ch >= 'a' && ch <= 'z') {
+				ch = (char) (ch - 32);
+			}
+			result = result + ch;
+		}
+		System.out.println(result);
+
+	}
+}
