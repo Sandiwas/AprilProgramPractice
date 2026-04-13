24425724cd5e30c6f21fed30c7ea946bc92a7e61
sandiwas <harrycode9@gmail.com>
2026-04-13 22:43:06 +0530

    added swap int values without using third value

---
+++ src/day8/SS_5_SwapTwoNumberWithout3rdVar.java
@@ -0,0 +1,15 @@
+package day8;
+
+public class SS_5_SwapTwoNumberWithout3rdVar {
+	public static void main(String[] args) {
+		int x = 100;
+		int y = 200;
+
+		x = x + y;
+		y = x - y;
+		x = x - y;
+
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+	}
+}
