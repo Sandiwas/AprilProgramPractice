2659dc8e69feeeba76d6473d3b3b7bc1f569325c
sandiwas <harrycode9@gmail.com>
2026-04-14 12:52:57 +0530

    find smallest number in array

---
+++ src/day9/SS_9_SamllestNumberInArray.java
@@ -0,0 +1,16 @@
+package day9;
+
+public class SS_9_SamllestNumberInArray {
+	public static void main(String[] args) {
+		int[] arr = { 5, 6, 4, 3, 2, 7, 1 };
+		int smallest = arr[0];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i]<smallest) {
+				smallest = arr[i];
+			}
+
+		}
+		System.out.println(smallest);
+	}
+}
