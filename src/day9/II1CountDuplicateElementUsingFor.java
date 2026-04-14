a790d9d4cc2336f692d9ca5281261a7791a5fad3
sandiwas <harrycode9@gmail.com>
2026-04-14 16:04:11 +0530

    count dulicate element in array

---
+++ src/day9/II1CountDuplicateElementUsingFor.java
@@ -0,0 +1,27 @@
+package day9;
+
+public class II1CountDuplicateElementUsingFor {
+	public static void main(String[] args) {
+		int[] arr = { 10, 20, 30, 10, 40, 50, 20, 30, 10 };
+		conutDuplicateEle(arr);
+	}
+
+	public static void conutDuplicateEle(int[] arr) {
+
+		for (int i = 0; i < arr.length; i++) {
+			int count = 1;
+			if (arr[i] == -1) {
+				continue;
+			}
+				for (int j = i + 1; j < arr.length; j++) {
+					if (arr[i] == arr[j]) {
+						count++;
+						arr[j] = -1;
+					}
+				}
+			if (count > 1 && arr[i] != -1) {
+				System.out.println(arr[i] + " = " + count);
+			}
+		}
+	}
+}
