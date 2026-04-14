4df8ade537c3eae636f4aefd8d89f113f80c3d24
sandiwas <harrycode9@gmail.com>
2026-04-14 11:41:47 +0530

    print factorial of number

---
+++ src/day9/SS_3_Factorial.java
@@ -0,0 +1,12 @@
+package day9;
+
+public class SS_3_Factorial {
+public static void main(String[] args) {
+	int n=5;
+	int fact=1;
+	for(int i=1;i<=n;i++) {
+		fact=fact*i;
+	}
+	System.out.println(fact+" ");
+}
+}
