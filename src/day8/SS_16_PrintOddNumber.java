6864b72461eefbe4bcde3b1afdceb14a145ff94e
sandiwas <harrycode9@gmail.com>
2026-04-14 10:27:52 +0530

    print odd number

---
+++ src/day8/SS_16_PrintOddNumber.java
@@ -0,0 +1,12 @@
+package day8;
+
+public class SS_16_PrintOddNumber {
+	public static void main(String[] args) {
+		int n = 100;
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
