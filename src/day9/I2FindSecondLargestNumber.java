be91d9ff3ea049f8c8eb6c3350287c842ca112a8
sandiwas <harrycode9@gmail.com>
2026-04-14 16:02:49 +0530

    added find second largest number in array

---
+++ src/day9/I2FindSecondLargestNumber.java
@@ -0,0 +1,28 @@
+package day9;
+
+public class I2FindSecondLargestNumber {
+	public static void main(String[] args) {
+		int arr[] = { 10, 15, 20, 26, 30, 10, 20 };
+		if (arr.length < 2) {
+			System.out.println("Invalid input");
+			return;
+		}
+
+		int largest = Integer.MIN_VALUE;
+		int second = Integer.MIN_VALUE;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > largest) {
+				second = largest;
+				largest = arr[i];
+			} else if (arr[i] > second && arr[i] != largest) {
+				second = arr[i];
+			}
+		}
+		if (second == Integer.MIN_VALUE) {
+			System.out.println("No Second largest number in array");
+		} else {
+			System.out.println(second);
+		}
+	}
+}
