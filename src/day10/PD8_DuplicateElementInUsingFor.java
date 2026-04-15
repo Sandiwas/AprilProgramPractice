b7ae6ecd19abccd6fe823595bf360e4786a63d6d
sandiwas <harrycode9@gmail.com>
2026-04-15 17:36:45 +0530

    print duplicate ele using for loop

---
+++ src/day10/PD8_DuplicateElementInUsingFor.java
@@ -0,0 +1,27 @@
+package day10;
+
+public class PD8_DuplicateElementInUsingFor {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 2, 6, 3, 1 };
+		CountDuplicateElement(arr);
+	}
+
+	public static void CountDuplicateElement(int[] arr) {
+
+		for (int i = 0; i < arr.length; i++) {
+			int count = 1;
+			if (arr[i] == -1) {
+				continue;
+			}
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					count++;
+					arr[j] = -1;
+				}
+			}
+			if (count > 1 && arr[i] != -1) {
+				System.out.println(arr[i] + " = " + count);
+			}
+		}
+	}
+}
