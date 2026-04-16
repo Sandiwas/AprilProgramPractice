70067c45eded4ac590e1b64f5b7b5570367a5eb4
sandiwas <harrycode9@gmail.com>
2026-04-16 12:01:24 +0530

    change logic of PP16 pattern program

--- src/day3/PP16.java
+++ src/day3/PP16.java
@@ -4,7 +4,7 @@ public class PP16 {
 public static void main(String[] args) {
 	for(int i=1;i<=4;i++) {
 		char ch=(i%2==0)? '&':'*';
-		for(int j=1;j<=i;j++) {
+		for(int j=4;j>=i;j--) {
 			System.out.print(ch);
 		}
 		System.out.println();
