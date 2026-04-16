e39c8e2ba20f9af36fc67fcc9eed083713d319b4
sandiwas <harrycode9@gmail.com>
2026-04-16 11:59:16 +0530

    pattern 2\2

---
+++ src/day10/PP22.java
@@ -0,0 +1,20 @@
+package day10;
+
+public class PP22 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 3; j >= i; j--) {
+				System.out.print(" ");
+			}
+			char ch = (i % 2 == 0) ? '*' : '&';
+			for (int k = 1; k <= i; k++) {
+				System.out.print(ch);
+			}
+			char ch1 = (i % 2 == 0) ? '*' : '&';
+			for (int l = 2; l <= i; l++) {
+				System.out.print(ch1);
+			}
+			System.out.println();
+		}
+	}
+}
