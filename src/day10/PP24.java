98ac5ae5684344f4c44602f64cf5ff227498a433
sandiwas <harrycode9@gmail.com>
2026-04-16 12:00:06 +0530

    pattern 24

---
+++ src/day10/PP24.java
@@ -0,0 +1,16 @@
+package day10;
+
+public class PP24 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 3; j >= i; j--) {
+				System.out.print(" ");
+			}
+			char ch = (i % 2 == 0) ? '*' : '&';
+			for (int k = 1; k <= i; k++) {
+				System.out.print(ch+" ");
+			}
+			System.out.println();
+		}
+	}
+}
