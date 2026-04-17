16cc5b6343dddc1ee307c8283f2e3306da66fc73
sandiwas <harrycode9@gmail.com>
2026-04-17 11:54:01 +0530

    make lower case

---
+++ src/day11/C1MakeLowerCaseString.java
@@ -0,0 +1,22 @@
+package day11;
+
+public class C1MakeLowerCaseString {
+	public static void main(String[] args) {
+
+		String str = "AuToMaTiOn";
+		System.out.println(makeLowerCase(str));
+	}
+
+	public static String makeLowerCase(String str) {
+		String result = "";
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (ch >= 'A' && ch <= 'Z') {
+				ch = (char) (ch + 32);
+			}
+			result = result + ch;
+		}
+		return result;
+
+	}
+}
