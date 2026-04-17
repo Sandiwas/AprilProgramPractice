9b36dfd6b1bd1b81ca1657df617cbcad06b0a01e
sandiwas <harrycode9@gmail.com>
2026-04-17 11:45:05 +0530

    reverse each word in sentence

---
+++ src/day10/P9ReversEachStringinSentence.java
@@ -0,0 +1,25 @@
+package day10;
+
+public class P9ReversEachStringinSentence {
+
+	public static void main(String[] args) {
+		String str = "Automation is fun";
+		System.out.println(reverseEachWord(str));
+	}
+
+	public static String reverseEachWord(String str) {
+		String[] arr = str.split(" ");
+		String result = "";
+		
+		for (int i = 0; i < arr.length; i++) {
+			String word = arr[i];
+			String reverseWord = "";
+			for (int j = word.length() - 1; j >= 0; j--) {
+				reverseWord = reverseWord + word.charAt(j);
+			}
+			result = result + reverseWord + " ";
+		}
+		return result;
+	}
+
+}
