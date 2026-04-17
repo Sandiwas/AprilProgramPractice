71d13e729cc63b9359eaf1baa272ba3f08c9b94a
sandiwas <harrycode9@gmail.com>
2026-04-17 14:54:53 +0530

    print alternate 0 to 9

---
+++ src/day11/C5Alternate0to9.java
@@ -0,0 +1,10 @@
+package day11;
+
+public class C5Alternate0to9 {
+	public static void main(String[] args) {
+		for (char ch = '0'; ch <= '9'; ch += 1) {
+			System.out.print(ch + " ");
+		}
+	}
+
+}
