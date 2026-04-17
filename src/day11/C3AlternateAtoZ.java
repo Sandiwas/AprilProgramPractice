8a423da34559121227e9f9a2c4802035dddad9cb
sandiwas <harrycode9@gmail.com>
2026-04-17 14:54:17 +0530

    print alternate a to z

---
+++ src/day11/C3AlternateAtoZ.java
@@ -0,0 +1,10 @@
+package day11;
+
+public class C3AlternateAtoZ {
+	public static void main(String[] args) {
+		for (char ch = 'A'; ch <= 'Z'; ch += 1) {
+			System.out.print(ch + " ");
+		}
+
+	}
+}
