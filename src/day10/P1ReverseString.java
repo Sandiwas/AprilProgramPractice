f8796a77c13a42b23cc8eb3c93971f0a2f680f2a
sandiwas <harrycode9@gmail.com>
2026-04-17 15:14:04 +0530

    reverse String

--- src/day10/P1ReverseString.java
+++ src/day10/P1ReverseString.java
@@ -1,7 +1,16 @@
 package day10;
 
 public class P1ReverseString {
-public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+		String str = "java";
+		System.out.println(reverseString(str));
+	}
+
+	public static String reverseString(String str) {
+		String result = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			result = result + str.charAt(i);
+		}
+		return result;
+	}
 }
