dfa1b70266d6a86374dd05ec4ae035d06d20780c
sandiwas <harrycode9@gmail.com>
2026-04-17 11:42:26 +0530

    added swap string with using third variable

---
+++ src/day10/P7SwapUsingThirdVaribale.java
@@ -0,0 +1,20 @@
+package day10;
+
+public class P7SwapUsingThirdVaribale {
+
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		String s2 = "world";
+		swapString(s1, s2);
+	}
+
+	public static void swapString(String s1, String s2) {
+		String s3 = "";
+		s3 = s1;
+		s1 = s2;
+		s2 = s3;
+		System.out.println("s1 = " + s1);
+		System.out.println("s2 = " + s2);
+	}
+
+}
