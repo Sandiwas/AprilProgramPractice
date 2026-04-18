5c83609b57d8cc8edc24a81e30d0d2dc8f5cca54
sandiwas <harrycode9@gmail.com>
2026-04-18 23:29:49 +0530

    modified bubble sort program

--- src/day7/I2BubbleSort.java
+++ src/day7/I2BubbleSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class I2BubbleSort {
 
 	public static void main(String[] args) {
-		int[] arr = { 5, 3, 4, 2 };
+		int[] arr = { 5, 3, 4, 2 ,1};
 		System.out.print(SortArray(arr));
 
 	}
