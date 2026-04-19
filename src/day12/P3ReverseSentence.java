2189a56f39290ecd293a1b10a30b94cfdcafdece
sandiwas <harrycode9@gmail.com>
2026-04-19 17:00:02 +0530

    print reverse sentence

---
+++ src/day12/P3ReverseSentence.java
@@ -0,0 +1,17 @@
+package day12;
+
+public class P3ReverseSentence {
+	public static void main(String[] args) {
+		String str = "Automation is fun";
+		System.out.println(reverseSentence(str));
+	}
+
+	public static String reverseSentence(String str) {
+		String[] arr = str.split(" ");
+		String result = "";
+		for (int i = arr.length - 1; i >= 0; i--) {
+			result = result + arr[i] + " ";
+		}
+		return result;
+	}
+}
