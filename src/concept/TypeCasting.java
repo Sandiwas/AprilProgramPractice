94e3edab6aff143ddc8fda8a4f8b77786b6194c9
sandiwas <harrycode9@gmail.com>
2026-04-20 21:23:11 +0530

    typecastring concept

---
+++ src/concept/TypeCasting.java
@@ -0,0 +1,15 @@
+package concept;
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		// implicit typecastring
+		int x = 'a';
+		System.out.println(x);
+
+		// explicite typecasting
+		int y = 130;
+		byte b = (byte) y;
+		System.out.println(b);
+
+	}
+}
