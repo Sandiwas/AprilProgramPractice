34703e0a54b2caf5460881a7588911791c8f0925
sandiwas <harrycode9@gmail.com>
2026-04-20 10:22:11 +0530

    added reverse sentence program

---
+++ src/day12/P4ReverseStringSB.java
@@ -0,0 +1,9 @@
+package day12;
+
+public class P4ReverseStringSB {
+	public static void main(String[] args) {
+		String str = "Automation";
+		StringBuffer sb = new StringBuffer(str);
+		System.out.println(sb.reverse());
+	}
+}
