cf50882effb276ec5eacd8beb35495af2a5fa54f
sandiwas <harrycode9@gmail.com>
2026-04-20 21:20:42 +0530

    post incerament concept

---
+++ src/concept/PostIncrement.java
@@ -0,0 +1,10 @@
+package concept;
+
+public class PostIncrement {
+public static void main(String[] args) {
+	int x = 10;
+	int y = x++;
+	System.out.println(x); // 11
+	System.out.println(y); // 10
+}
+}
