6a52e89183d5b128a7ada4d8dcb203219175cf84
sandiwas <harrycode9@gmail.com>
2026-04-20 10:25:40 +0530

    reverse sentence

---
+++ src/day12/P10ReverseSentenceAndEachString.java
@@ -0,0 +1,23 @@
+package day12;
+
+public class P10ReverseSentenceAndEachString {
+	public static void main(String[] args) {
+		String str = "Automation is fun";
+		System.out.println(reverseEachWordInSentence(str));
+	}
+
+	public static String reverseEachWordInSentence(String str) {
+		String[] arr = str.split(" ");
+		String result = "";
+		for (int i = arr.length - 1; i >= 0; i--) {
+			String word = arr[i];
+			String reverseWord = "";
+			for (int j = word.length() - 1; j >= 0; j--) {
+				reverseWord = reverseWord + word.charAt(j);
+			}
+			result = result + reverseWord + " ";
+		}
+		return result;
+	}
+
+}
