51f63f43bb32b18a4134f746c2cf49445a58c629
sandiwas <harrycode9@gmail.com>
2026-04-21 10:21:03 +0530

    substring program

---
+++ src/day13/A8_ExtractString.java
@@ -0,0 +1,16 @@
+package day13;
+
+public class A8_ExtractString {
+	public static void main(String[] args) {
+		String str = "www.erostrom.com";
+
+		String firstFour = str.substring(0, 4);
+		String lastFour = str.substring(str.length() - 4, str.length());
+		String websiteName = str.substring(4, str.length() - 4);
+
+		System.out.println(firstFour);
+		System.out.println(lastFour);
+		System.out.println(websiteName);
+
+	}
+}
