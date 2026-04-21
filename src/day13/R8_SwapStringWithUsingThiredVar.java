393d23169a545d601a776a53b1e1c7f43b67422d
sandiwas <harrycode9@gmail.com>
2026-04-21 10:25:39 +0530

    swap string

---
+++ src/day13/R8_SwapStringWithUsingThiredVar.java
@@ -0,0 +1,17 @@
+package day13;
+
+public class R8_SwapStringWithUsingThiredVar {
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		String s2 = "World";
+		swap(s1, s2);
+	}
+
+	public static void swap(String s1, String s2) {
+		s1 = s1 + s2;
+		s2 = s1.substring(0, s1.length() - s2.length());
+		s1 = s1.substring(s2.length());
+		System.out.println("s1 = " + s1);
+		System.out.println("s2 = " + s2);
+	}
+}
