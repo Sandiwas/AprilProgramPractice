9d0dd6a5664c3f848fd47f07d0c02342827faae7
sandiwas <harrycode9@gmail.com>
2026-04-21 22:01:15 +0530

    added Reverse each string sentence

---
+++ src/day13/P9ReversEachStringinSentence.java
@@ -0,0 +1,23 @@
+package day13;
+
+public class P9ReversEachStringinSentence {
+	public static void main(String[] args) {
+		String str = "Automation is fun";
+		System.out.println(reverseString(str));
+	}
+
+	public static String reverseString(String str) {
+		String[] arr = str.split(" ");
+		String result = "";
+		for (int i = 0; i < arr.length; i++) {
+			String word = arr[i];
+			String reverse = "";
+			for (int j = word.length() - 1; j >= 0; j--) {
+				reverse = reverse + word.charAt(j);
+			}
+			result = result + reverse + " ";
+		}
+		return result;
+
+	}
+}
