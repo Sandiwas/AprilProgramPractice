35f53663359bf80baf62854720901be5f3565cc1
sandiwas <harrycode9@gmail.com>
2026-04-21 10:23:47 +0530

    reverse sentence

---
+++ src/day13/R4_ReverseStringUsingSB.java
@@ -0,0 +1,9 @@
+package day13;
+
+public class R4_ReverseStringUsingSB {
+	public static void main(String[] args) {
+		String str = "java";
+		StringBuffer sb = new StringBuffer(str);
+		System.out.println(sb.reverse());
+	}
+}
